7c692bf1e6780c3b5655efbf19be6eade02391d4
StriverFive <72422845+StriverFive@users.noreply.github.com>
2020-10-06 06:46:05 +0530

    Create check_palindrome.py

---
+++ check_palindrome.py
@@ -0,0 +1,11 @@
+def check_palindrome(number):
+  """Return True if number is palindrome otherwise return False"""
+  pass
+  
+  
+if  __name__ == "__main__":
+  n = int(input())
+  if check_palindrome(n):
+    print("{} is Palindrome.".format(n))
+  else:
+    print("{} is not Palindrome.".format(n))
